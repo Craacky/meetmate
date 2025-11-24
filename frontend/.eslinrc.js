9c98b207a65866867342a7db97fbd27e0d8ad07f
Craacky <cbxj.ysa@gmail.com>
2025-11-24 13:01:26 +0300

    create skeleton of app

---
+++ frontend/.eslinrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier',
+	],
+	settings: { react: { version: 'detect' } },
+};
